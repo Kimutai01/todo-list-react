b83a0b21e2d4edad17340617d87ed930f40955cc
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-07-04 15:00:45 +0100

    passing events from child to parent

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,15 +1,13 @@
-// export class TodoItem extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <h1>{this.props.todo.title}</h1>
-//       </div>
-//     );
-//   }
-// }
-
-// export default TodoItem;
 import React from "react";
-const TodoItem = (props) => <li> {props.todo.title}</li>;
+const TodoItem = (props) => (
+  <li>
+    <input
+      type="checkbox"
+      checked={props.todo.checked}
+      onChange={() => props.handleChangeProps()}
+    />{" "}
+    {props.todo.title}
+  </li>
+);
 
 export default TodoItem;
